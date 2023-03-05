bb7607c4b8d1a9448c894b02af340733d6d9c420
ejlde <122563402+ejlde@users.noreply.github.com>
2023-03-05 15:06:51 -0700

    ML One

---
+++ ML/ML_1.py
@@ -0,0 +1,15 @@
+# What is machine learning?
+# Teaching a cp how to learn based on data
+
+## Supervised Learning
+# input cats and dogs - 
+# watch if cp returns cat and dog correctly
+
+## Unsupervised Learning
+# Input weight - height graph
+# No classifications - look for patterns
+# CP returns clusters of data w/o labels
+
+## Reinforced Learning
+# Agents and environments
+# Assign points for cp performing desired actions
