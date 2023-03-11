b259c0273f0c44426a377c755ccbc5d0d1a4e6a3
ejlde <122563402+ejlde@users.noreply.github.com>
2023-03-10 21:56:27 -0700

    Neural Net

---
+++ Finances/main.py
@@ -0,0 +1,21 @@
+from neuralintents import GenericAssistant
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import mplfinance as mpf
+
+import pickle
+import sys
+import datetime as dt
+
+# def myfunction():
+#     pass
+
+# mappings = {
+#     'greetings' # need to specify the tag from json
+# }
+# assistant = GenericAssistant('intents.json',intent_methods=mappings)
+
+# assistant.train_model()
+
+# assistant.request()
--- ML/ML_7_Neural_Networks.py
+++ ML/ML_7_Neural_Networks.py
@@ -8,14 +8,51 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # Already know the classifications of all the digits
-mnist = tf.keras.datasets.mnist
-(x_train,y_train),(x_test,ytest) = mnist.load_data()
+# loading in data - already know classifications
+mnist = tf.keras.datasets.mnist # mnist - handwritten digits dataset
 
+# split into training, and testing data
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+# scaling down data (normalization) - y data = labels - dont want to scale
 x_train = tf.keras.utils.normalize(x_train,axis=1)
 x_test = tf.keras.utils.normalize(x_test,axis=1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+# Define the model
+
+# Take an input layer  
+model = tf.keras.models.Sequential() # create a feedforward NN
+# Adding a new layer
+model.add(tf.keras.layers.Flatten(input_shape=(28,28))) # 1D layer 28x28=pixels of image
+# Dense Layer -  1st hidden layer
+model.add(tf.keras.layers.Dense(units = 128, activation = tf.nn.relu)) # all neurons are connected to prev and next layer
+# units = neurons - more = more sophisticated - activation = rectify linear unit
+
+# 2nd hidden layer
 model.add(tf.keras.layers.Dense(units = 128, activation = tf.nn.relu))
-model.add(tf.keras.layers.Dense(units = 128, activation = tf.nn.relu))
-model.add(tf.keras.Dense(units=10,activation=tf.nn.softmax))
+
+# Output layer
+model.add(tf.keras.layers.Dense(units=10,activation=tf.nn.softmax)) # softmax activation function
+# softmax scales values down to - returns result probabilities
+
+
+# Compilation - 
+model.compile(optimizer = 'adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(x_train,y_train,epochs=3)
+# epochs = how many times will the data be seen over again
+
+accuracy, loss = model.evaluate(x_test,y_test)
+print(accuracy)
+print(loss)
+
+model.save('digits.model')
+
+for x in range(1,4):
+    img = cv.imread(f'{x}.png')[:,:,0]
+    img = np.invert(np.array([img])) # invert changes to black on white digits
+    prediction = model.predict(img)
+    print(f' The result is probably: {np.argmax(prediction)}')
+    plt.imshow(img[0],cmap = plt.cm.binary)
+    plt.show()
+
