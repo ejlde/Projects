eb4e64e37dd8f7b322b528c4f74a66ad93d02396
ejlde <122563402+ejlde@users.noreply.github.com>
2024-09-20 13:07:36 -0700

    day 33 api get requests (#3)

--- udemy/day33-api/api/sunset-main.py
+++ udemy/day33-api/api/sunset-main.py
@@ -1,15 +1,20 @@
 import requests
-
+from datetime import datetime,timezone
 
 MY_LAT = 33.797119
 MY_LONG = -118.168358
 parameters = {
     "lat":MY_LAT,
     "lng":MY_LONG,
+    "formatted": 0
 }
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
 sunrise = data["results"]["sunrise"]
 sunset = data["results"]["sunset"]
-print(data)
+print(sunrise)
+
+time_now  = datetime.now()
+
+print(time_now)
