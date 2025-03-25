b688b97f1a2d0fb311a3069516e8981b6b2bf5ca
Eric Dean <ejldean13@gmail.com>
2025-03-25 15:30:00 -0700

    structs and pointers

---
+++ go/structs/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	// Give me the memory address of this var
+	// value to address & pointer = &jim
+	jim.updateName("jimmy")
+	jim.print()
+
+}
+
+// *pointer give me the value this memory address is pointing at
+// *person = pointer to a person type not an operator
+func (pointerToPerson *person) updateName(newFirstaname string) {
+	(*pointerToPerson).firstName = newFirstaname // address to value *
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
