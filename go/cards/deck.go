f063f70400b22b318186c5905a10fec619181831
Eric Dean <ejldean13@gmail.com>
2025-03-25 12:03:33 -0700

    shuffling, rand nums, seed generation, and go mod init cards

--- go/cards/deck.go
+++ go/cards/deck.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -55,4 +57,17 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	// Creating a new seed for the random num generator
+	source := rand.NewSource(time.Now().UnixNano()) // different int64 every run
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i] // swapping element indicies
+
+	}
 }
